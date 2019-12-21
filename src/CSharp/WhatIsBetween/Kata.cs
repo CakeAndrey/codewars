715137e1153d967bcfa08da8de402739efe84b09
Andrei Lugovoi <andrei.lugovoi@wowkids.io>
2019-12-21 21:53:41 +0200

    Added C# implementation of WhatIsBetween kata

---
+++ src/CSharp/WhatIsBetween/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsBetween
+{
+    public class Kata
+    {
+        public static int[] Between(int a, int b) =>
+            Enumerable.Range(a, b - a + 1).ToArray();
+    }
+}
