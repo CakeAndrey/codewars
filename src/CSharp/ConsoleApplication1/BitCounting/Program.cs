c45d3f16a9aa4a551ba1aba9fcf95f77cd6625c7
Andrei Lugovoi <andrei.lugovoi.developer@gmail.com>
2021-05-08 11:42:53 +0300

    Refactor file names to match class names.

--- src/CSharp/ConsoleApplication1/BitCounting/Program.cs
+++ src/CSharp/ConsoleApplication1/BitCounting/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Linq.Enumerable;
+﻿using static System.Linq.Enumerable;
 
 namespace BitCounting
 {
