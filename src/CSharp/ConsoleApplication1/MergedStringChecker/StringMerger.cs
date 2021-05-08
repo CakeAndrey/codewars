768277a7224121ee29c75eaa7d3b27412feb7265
Andrei Lugovoi <andrei.lugovoi.developer@gmail.com>
2021-05-08 11:36:38 +0300

    Add resharper to gitignore.

---
+++ src/CSharp/ConsoleApplication1/MergedStringChecker/StringMerger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MergedStringChecker
+{
+    class StringMerger
+    {
+        static void Kata(string[] args)
+        {
+            isMerge("Bananas from Bahamas", "Bahas", "Bananas from am");
+            Console.ReadKey();
+        }
+
+        public static bool isMerge(string s, string part1, string part2)
+        {
+            Console.WriteLine(s);
+            Console.WriteLine(part1);
+            Console.WriteLine(part2);
+
+            Console.WriteLine();
+            throw new NotImplementedException("Finding the best solution to implement this using linq.");
+
+        }
+
+
+
+    }
+}
